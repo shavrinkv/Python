b34e5b910c048f72d206b9bb4306c5b423db6c4f
ваше_имя <shavrinkv@yandex.ru>
2023-10-06 00:44:34 +0300

    first commit

--- hw5/03.py
+++ hw5/03.py
@@ -2,4 +2,4 @@ import numpy as np
 import pandas as pd
 df = pd.DataFrame(np.random.randint(0,100,size=(10, 10)), columns=list('ABCDEFGHKL'))
 df.index = ['Row_1', 'Row_2', 'Row_3', 'Row_4', 'Row_5', 'Row_6', 'Row_7', 'Row_8', 'Row_9', 'Row_10']
-print(df)
+print(df
