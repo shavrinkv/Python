a417c98642118b8f4c80d52f5284a8a95c4ccef7
ваше_имя <shavrinkv@yandex.ru>
2023-10-10 00:23:31 +0300

    commit 2

--- hw5/03.py
+++ hw5/03.py
@@ -2,7 +2,23 @@ import numpy as np
 import pandas as pd
 df = pd.DataFrame(np.random.randint(0,100,size=(10, 10)), columns=list('ABCDEFGHKL'))
 df.index = ['Row_1', 'Row_2', 'Row_3', 'Row_4', 'Row_5', 'Row_6', 'Row_7', 'Row_8', 'Row_9', 'Row_10']
+print(df.shape)
 print(df)
+print(df.A, df.B)
+
+
+mean1 = df.mean(axis=0)
+mean2 = df.mean(axis=1)
+print(mean1, mean2)
+
+
+df.to_csv('file1')
+
+
+
+
+
+
 
 
 
---
+++ hw5/04.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+read1 = pd.read_csv('emojis.csv')
+print(read1)
